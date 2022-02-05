01d7e9293d3f8245067a1ecf011ee4eff1d7c165
John Savely <jh.savely@gmail.com>
2022-02-05 02:15:48 +0200

    initial

---
+++ src/main/java/com/jsavely/IPZBase/IpzBaseApplication.java
@@ -0,0 +1,20 @@
+package com.jsavely.IPZBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class IpzBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpzBaseApplication.class, args);
+	}
+
+	@GetMapping
+	public String msg(){
+		return "Spring returned String!";
+	}
+}
---
+++ src/test/java/com/jsavely/IPZBase/IpzBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsavely.IPZBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpzBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
