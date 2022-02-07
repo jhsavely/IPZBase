746787716bfda7dd36b21ca39ff2d615e03a16f1
John Savely <jh.savely@gmail.com>
2022-02-07 21:29:51 +0200

    added: /home with greetings by param, textarea on teachers

--- src/main/java/com/jsavely/IPZBase/FirstController.java
+++ src/main/java/com/jsavely/IPZBase/FirstController.java
@@ -3,13 +3,17 @@ package com.jsavely.IPZBase;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController{
 
     @GetMapping("/home")
-	public String getMsg(Model model){
-        model.addAttribute("msg", "this is a message from a controller");
+	public String getMsg(
+        @RequestParam(name = "name", required = false, defaultValue = " ello") 
+        String msg,
+        Model model){
+        model.addAttribute("msg", msg);
 		return "first";
 	}
 }
