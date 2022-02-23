7ad79c99a52d9da5ed20b9faa4e1f029f9fae974
John Savely <jh.savely@gmail.com>
2022-02-23 03:32:18 +0200

    added: CRUD finished and works, test.http for req.

--- src/main/java/com/jsavely/IPZBase/StudentController.java
+++ src/main/java/com/jsavely/IPZBase/StudentController.java
@@ -3,8 +3,14 @@ package com.jsavely.IPZBase;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
     private final StudentService studentService;
+    
 
     @Autowired 
     public StudentController(StudentService studentService) {
@@ -21,6 +28,26 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudents(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+
+        studentService.updateStudent(studentId, name, email);
     }
     
 }
--- src/main/java/com/jsavely/IPZBase/StudentService.java
+++ src/main/java/com/jsavely/IPZBase/StudentService.java
@@ -1,10 +1,12 @@
 package com.jsavely.IPZBase;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -19,13 +21,41 @@ public class StudentService {
     public List<Student> getStudents(){
         return  studentRepository.findAll();  
     }
-}
 
-// List.of(
-//             new Student(
-//                 1L,
-//                 "John",
-//                 LocalDate.of(2000, 11, 4),
-//                 "any@fn.mail"
-//             )
-//         );
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("student with id" + studentId + " doesn't exist"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
+    }
+}
